50e6b86b58768dd7dc41f19aae594d369b51b645
Mario <94_mario@hotmail.es>
2019-02-20 23:36:23 +0100

    Dataset fuera del proyecto

--- codigo.py
+++ codigo.py
@@ -4,9 +4,9 @@ import numpy as np
 
 ## IMPORTS ##
 
-X_tr = scipy.sparse.load_npz('./dataset/X_tr.npz')
-X_tst = scipy.sparse.load_npz('./dataset/X_tst.npz')
-y_tr = np.load('./dataset/y_tr.npy')
-y_tst = np.load('./dataset/y_tst.npy')
+X_tr = scipy.sparse.load_npz('../Datasets/dataset/X_tr.npz')
+X_tst = scipy.sparse.load_npz('../Datasets/dataset/X_tst.npz')
+y_tr = np.load('../Datasets/dataset/y_tr.npy')
+y_tst = np.load('../Datasets/dataset/y_tst.npy')
 print (np.shape(y_tr))
 print (y_tr[1345])

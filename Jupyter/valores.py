6ad4f83fa31ceda050696fa84e8cc7e71871dd07
Mario Trapiello Mendoza <mariotrapiellomendoza@iMac-de-Mario.local>
2019-06-25 00:58:52 +0200

    Version Final

--- Jupyter/valores.py
+++ Jupyter/valores.py
@@ -11,65 +11,67 @@ clasificadores_dict ={
 }
 
 parameters0 = {
-    "estimator__C": [1],
+    "estimator__C": [1,10,100,1000],
     "estimator__solver": ["liblinear"],
-    "estimator__tol": [0.01]
+    "estimator__tol": [0.1,0.01,0.001,0.0001]
 }
 parameters1 = {
-    "base_estimator__C": [4],
+    "base_estimator__C": [1,4,10,100,1000],
     "base_estimator__solver": ["liblinear"],
-    "base_estimator__tol": [0.01],
+    "base_estimator__tol": [0.1,0.01,0.001,0.0001],
+    "base_estimator__max_iter":[10000]
 }
 parameters2 = {
-    "estimator__alpha": [1e-8],
-    "estimator__tol": [0.001],
-    "estimator__warm_start": [False],
+    "estimator__alpha": [5e-5,1e-7,1e-8],
+    "estimator__tol": [0.01,0.001,0.0001],
+    "estimator__warm_start": [False,True],
     "estimator__random_state" :[42],
     "estimator__learning_rate" :["invscaling"],
-    "estimator__eta0":[1],
-    "estimator__power_t":[0.3]
+    "estimator__eta0":[1,3,5,10],
+    "estimator__power_t":[0.1,0.2,0.3,0.4,0.5,0.6]
 }
 parameters3 = {
-    "base_estimator__alpha": [1e-7],
-    "base_estimator__tol": [0.01],
-    "base_estimator__warm_start": [False],
+    "base_estimator__alpha": [5e-5,1e-7,1e-8],
+    "base_estimator__tol": [0.01,0.001,0.0001],
+    "base_estimator__warm_start": [False,True],
     "base_estimator__random_state" :[42],
     "base_estimator__learning_rate" :["invscaling"],
-    "base_estimator__eta0":[3],
-    "base_estimator__power_t":[0.4]
+    "base_estimator__eta0":[1,3,5,10],
+    "base_estimator__power_t":[0.1,0.2,0.3,0.4,0.5,0.6]
 }
 parameters4 = {
-    "estimator__alpha": [1e-1],
+    "estimator__penalty":[None],
+    "estimator__alpha": [1e-1,1e-2,1e-3,1e-4,1e-5,1e-6,1e-7],
     "estimator__tol": [0.01],
     "estimator__warm_start": [False],
-    "estimator__eta0":[1],
-    "estimator__shuffle":[True]
+    "estimator__eta0":[100,10,1,0.1],
+    "estimator__shuffle":[False]
 }
 parameters5 = {
     "base_estimator__penalty":[None],
-    "base_estimator__alpha": [0.1],
-    "base_estimator__tol": [0.01],
-    "base_estimator__warm_start": [False],
-    "base_estimator__eta0":[1],
-    "base_estimator__shuffle":[True]
+    "base_estimator__alpha": [1e-1,1e-2,1e-3,1e-4,1e-5,1e-6,1e-7],
+    "base_estimator__tol": [0.01,0.001,0.0001],
+    "base_estimator__warm_start": [False, True],
+    "base_estimator__eta0":[1,3,5,10],
+    "base_estimator__shuffle":[True,False]
 }
 parameters6 = {
-    "estimator__C":[0.1],
-    "estimator__tol": [0.1],
-    "estimator__warm_start": [False],
+    "estimator__C":[0.1,1,10,100],
+    "estimator__tol": [0.01,],
+    "estimator__warm_start": [True,False],
     "estimator__class_weight":[None],
     "estimator__shuffle":[True],
-    "estimator__average": [False],
+    "estimator__average": [True],
     "estimator__loss" : ["hinge"]
 }
 parameters7 = {
-    "base_estimator__C":[0.1],
-    "base_estimator__tol": [0.01],
-    "base_estimator__warm_start": [True],
+    "base_estimator__C":[0.1,1,10,100],
+    "base_estimator__tol": [0.1,0.01,0.001],
+    "base_estimator__warm_start": [True,False],
     "base_estimator__class_weight":[None],
-    "base_estimator__shuffle":[True],
-    "base_estimator__average": [10],
-    "base_estimator__loss" : ["hinge"]
+    "base_estimator__shuffle":[True,False],
+    "base_estimator__average": [True,False,10,50,100],
+    "base_estimator__loss" : ["hinge","squared_hinge"]
 }
 
 
